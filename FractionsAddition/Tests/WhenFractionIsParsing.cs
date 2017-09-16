f295bccf14210ff2959baa06ebb6a4fcc6d138f0
Sergey Rubtsov <rubtsov.sergey.v@gmail.com>
2017-09-16 09:39:59 +0600

    Start TDD

---
+++ FractionsAddition/Tests/WhenFractionIsParsing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class WhenFractionIsParsing
+    {
+        [Fact]
+        public void ParseSimpleFraction()
+        {
+            var stringFraction = "1/2";
+            var fraction = new Fraction(stringFraction);
+
+            Assert.Equal(1, fraction.Numerator);
+            Assert.Equal(2, fraction.Denominator);
+        }
+    }
+
+    public class Fraction
+    {
+        public Fraction(string stringFraction)
+        {
+            Numerator = 1;
+            Denominator = 2;
+        }
+
+        /// <summary>
+        /// Числитель
+        /// </summary>
+        public int Numerator { get; private set; }
+        /// <summary>
+        /// Знаменатель
+        /// </summary>
+        public int Denominator { get; private set; }
+    }
+}
